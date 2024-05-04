b32b6f5a59038230148fccd71177a9033f570081
Sav Giu <silice135@gmail.com>
2024-05-04 11:02:00 +0200

    data producer is now linked to openweathermap API to grab real data with Python script meteo_from_openweather.py; the city is specified into config.toml file; all activities of logged into log_file.log; changed the docker file to include bind mount volume for config and log file

--- app/garden_sensor_gateway.py
+++ app/garden_sensor_gateway.py
@@ -1,41 +1,49 @@
 import time
 import json
-import random
-
 from kafka import KafkaProducer
+import requests
 
-
-def random_temp_cels():
-    return round(random.uniform(-10, 50), 1)
-
-def random_humidity():
-    return round(random.uniform(0, 100), 1)
-
-def random_wind():
-    return round(random.uniform(0, 10), 1)
-
-def random_soil():
-    return round(random.uniform(0, 100), 1)
-
-def get_json_data():
-    data = {}
-
-    data["temperature"] = random_temp_cels()
-    data["humidity"] = random_humidity()
-    data["wind"] = random_wind()
-    data["soil"] = random_soil()
-
-    return json.dumps(data) 
+def get_weather(api_key, city):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        weather_info = {
+            "temperature": data["main"]["temp"],
+            "description": data["weather"][0]["description"],
+            "humidity": data["main"]["humidity"],
+            "wind_speed": data["wind"]["speed"],
+            "city": data["name"],
+            "country": data["sys"]["country"]
+        }
+        return weather_info
+    else:
+        print("Failed to fetch weather data:", response.status_code)
+        return None
 
 def main():
-    producer = KafkaProducer(bootstrap_servers=['kafka:9092'])
+    api_key = "5bc266ed5946232b512e614896a75f37"  # Replace "YOUR_API_KEY" with your actual API key
+    while True:
+        city = "Bari"
+        weather = get_weather(api_key, city)
+        if weather:
+            print("Weather in {}, {}: ".format(weather["city"], weather["country"]))
+            print("Temperature: {}°C".format(weather["temperature"]))
+            print("Description:", weather["description"])
+            print("Humidity:", weather["humidity"], "%")
+            print("Wind Speed:", weather["wind_speed"], "m/s")
 
-    for _ in range(20000):
-        json_data = get_json_data()
-        producer.send('garden_sensor_data', bytes(f'{json_data}','UTF-8'))
-        print(f"Sensor data is sent: {json_data}")
+            json_data = json.dumps(weather)
+            # Transmit data to kafka
+            producer = KafkaProducer(bootstrap_servers=['kafka:9092'])
+
+            producer.send('garden_sensor_data', bytes(f'{json_data}', 'UTF-8'))
+            print(f"Sensor data is sent: {json_data}")
+
+        else:
+            print("Failed to fetch weather data.")
         time.sleep(5)
-
 
 if __name__ == "__main__":
     main()
+
